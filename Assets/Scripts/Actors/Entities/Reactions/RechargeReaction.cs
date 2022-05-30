10191a75f350734380e71df4cd2067e883da3046
MaminProgrammist <kit04048@gmail.com>
2022-05-30 15:56:23 +0300

    Добавлена реакция
    Новая реакция перезарядки

---
+++ Assets/Scripts/Actors/Entities/Reactions/RechargeReaction.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts
+{
+    class RechargeReaction : Reaction
+    {
+        private Charger _charger;
+
+        public RechargeReaction(Quadcopter quadcopter)
+        {
+            _charger = quadcopter.GetComponent<Charger>();
+        }
+
+        public override void React()
+        {
+            _charger.ChargeUp();
+        }
+    }
+}
