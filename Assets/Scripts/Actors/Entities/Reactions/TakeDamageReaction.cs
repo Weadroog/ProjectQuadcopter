a18783e65d812e41273fbcbe5a620e6a7e04d42b
MaminProgrammist <kit04048@gmail.com>
2022-05-30 15:57:16 +0300

    Добавлена реакция
    Новая реакция получения урона

---
+++ Assets/Scripts/Actors/Entities/Reactions/TakeDamageReaction.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts
+{
+    public class TakeDamageReaction : Reaction
+    {
+        private Liver _lives;
+
+        public TakeDamageReaction(Liver lives)
+        {
+            _lives = lives;
+        }
+
+        public override void React()
+        {
+            _lives.TakeDamage();
+        }
+    }
+}
