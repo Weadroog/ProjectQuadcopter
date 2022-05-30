86aa88086a592dd70fd76f6fa6cf48108ca7c380
MaminProgrammist <kit04048@gmail.com>
2022-05-30 15:56:01 +0300

    Рефакторинг кода
    Изменены скрипты фабрики и конфига коптера

--- Assets/Scripts/Actors/Entities/Quadcopter/QuadcopterConfig.cs
+++ Assets/Scripts/Actors/Entities/Quadcopter/QuadcopterConfig.cs
@@ -5,10 +5,14 @@ namespace Assets.Scripts
     [CreateAssetMenu(menuName = "Config/Quadcopter", fileName = "New Quadcopter Config")]
     public class QuadcopterConfig : ActorConfig<Quadcopter> 
     {
-        [SerializeField][Range(0, 1)] private float _motionDuration;
-        [SerializeField] [Range(1, 5)] private int _HP;
+        [SerializeField] [Range(0, 1)] private float _motionDuration;
+        [SerializeField] [Range(1, 5)] private int _lives;
+        [SerializeField] [Range(1, 5)] private int _charge;
+        [SerializeField] [Range(1, 15)] private int _chargeDecreaseTime;
 
-        public int HP { get =>_HP; }
+        public int Lives => _lives;
+        public int Charge => _charge;
+        public int ChargeDecreaseTime => _chargeDecreaseTime;
         public float MotionDuration => _motionDuration;
     }
 }
--- Assets/Scripts/Actors/Entities/Quadcopter/QuadcopterFactory.cs
+++ Assets/Scripts/Actors/Entities/Quadcopter/QuadcopterFactory.cs
@@ -4,17 +4,33 @@ namespace Assets.Scripts
 {
     class QuadcopterFactory : EntityFactory<Quadcopter, QuadcopterConfig>
     {
-        public QuadcopterFactory(QuadcopterConfig config, Container container) : base(config, container) { }
+        private LifeCounter _lifeCounter;
+        private ChargeCounter _chargeCounter;
+
+        public QuadcopterFactory(QuadcopterConfig config, Container container, LifeCounter lifeCounter, ChargeCounter chargeCounter) : base(config, container) 
+        {
+            _lifeCounter = lifeCounter;
+            _chargeCounter = chargeCounter;
+        }
 
         public override Quadcopter GetCreated()
         {
             Quadcopter quadcopter = Object.Instantiate(_config.Prefab, _container.transform);
             SwipeController swipeController = quadcopter.gameObject.AddComponent<SwipeController>();
-            Liver health = quadcopter.gameObject.AddComponent<Liver>();
-            quadcopter.AddReaction<CollisionDetector, NetGuy>(new FreezingReaction());
+            Liver liver = quadcopter.gameObject.AddComponent<Liver>();
+            Charger charger = quadcopter.gameObject.AddComponent<Charger>();
+            quadcopter.AddReaction<CollisionDetector, NetGuy, AggressiveBird, Car>(new TakeDamageReaction(liver));
+            quadcopter.AddReaction<CollisionDetector, Battery>(new RechargeReaction(quadcopter));
             swipeController.SetStartablePosition(MatrixPosition.Center);
             swipeController.SetMotionDuration(_config.MotionDuration);
-            health.SetMaxLives(_config.HP);
+            liver.OnChanged += _lifeCounter.Display;
+            liver.SetMaxLives(_config.Lives);
+            liver.ResetHP();
+            charger.OnChanged += _chargeCounter.Display;
+            charger.SetMaxCharge(_config.Charge);
+            charger.SetDecreaseTime(_config.ChargeDecreaseTime);
+            charger.ChargeUp();
+
             return quadcopter;
         }
     }
