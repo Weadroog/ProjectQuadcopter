34ad717ad607015a807b84da2f919094335ea910
MaminProgrammist <kit04048@gmail.com>
2022-05-30 15:44:44 +0300

    Новая фабрика
    Добавлена фабрика батареек

---
+++ Assets/Scripts/Actors/Entities/Battery/BatteryFactory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class BatteryFactory : EntityFactory<Battery, BatteryConfig>
+    {
+        private WayMatrix _wayMatrix = new();
+
+        public BatteryFactory(BatteryConfig config) : base(config) { }
+
+        public override Battery GetCreated()
+        {
+            Battery battery = Object.Instantiate(_config.Prefab); 
+            Disappearer disappearer = battery.gameObject.AddComponent<Disappearer>();
+            battery.gameObject.AddComponent<Mover>();
+            disappearer.SetDisappearPoint(_wayMatrix.DisappearPoint);
+            return battery;
+        }
+    }
+}
