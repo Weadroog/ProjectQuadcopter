306d18359cb776ed689f0320a78d937ee1ae38e7
Matroskin_Cat <groznov3@gmail.com>
2022-05-30 19:50:58 +0300

    Добавление перегрузок к методу добавления реакции

--- Assets/Scripts/Actors/Entities/Entity.cs
+++ Assets/Scripts/Actors/Entities/Entity.cs
@@ -6,7 +6,7 @@ namespace Assets.Scripts
 {
     public abstract class Entity : Actor 
     {
-        private Dictionary<Type, IDetector> _detectors = new Dictionary<Type, IDetector>();
+        protected Dictionary<Type, IDetector> _detectors = new Dictionary<Type, IDetector>();
 
         public D AddReaction<D>(Reaction reaction) where D : Component
         {
@@ -44,6 +44,31 @@ namespace Assets.Scripts
             _detectors[typeof(D)].OnDetect += reaction.TryToReact<E1>;
             _detectors[typeof(D)].OnDetect += reaction.TryToReact<E2>;
             _detectors[typeof(D)].OnDetect += reaction.TryToReact<E3>;
+            return _detectors[typeof(D)] as D;
+        }
+
+        public D AddReaction<D, E1, E2, E3, E4>(Reaction reaction) where D : Component where E1 : Entity where E2 : Entity where E3 : Entity where E4 : Entity
+        {
+            if (_detectors.ContainsKey(typeof(D)) == false)
+                _detectors.Add(typeof(D), gameObject.AddComponent<D>() as IDetector);
+
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E1>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E2>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E3>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E4>;
+            return _detectors[typeof(D)] as D;
+        }
+
+        public D AddReaction<D, E1, E2, E3, E4, E5>(Reaction reaction) where D : Component where E1 : Entity where E2 : Entity where E3 : Entity where E4 : Entity where E5 : Entity
+        {
+            if (_detectors.ContainsKey(typeof(D)) == false)
+                _detectors.Add(typeof(D), gameObject.AddComponent<D>() as IDetector);
+
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E1>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E2>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E3>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E4>;
+            _detectors[typeof(D)].OnDetect += reaction.TryToReact<E5>;
             return _detectors[typeof(D)] as D;
         }
     }
