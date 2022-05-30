b367754c5379618bc9510a274d880f8a53d8e1b6
Matroskin_Cat <groznov3@gmail.com>
2022-05-30 11:43:56 +0300

    Переработка фабрики актёров

--- Assets/Scripts/Actors/ActorFactory.cs
+++ Assets/Scripts/Actors/ActorFactory.cs
@@ -2,24 +2,22 @@
 
 namespace Assets.Scripts
 {
-    public abstract class ActorFactory<A, C> : IFactory<A> where A : Actor where C : Config
+    public abstract class ActorFactory<A> : IFactory<A> where A : Actor
     {
-        protected C _config;
+        
         protected Container _container;
         protected Vector3 _spawnPosition;
 
-        public ActorFactory(C config) => _config = config;
+        public ActorFactory() { }
 
-        public ActorFactory(C config, Container container)
+        public ActorFactory(Container container)
         {
-            _config = config;
             _container = container;
             _spawnPosition = container.transform.position;
         }
 
-        public ActorFactory(C config, Container container, Vector3 spawnPosition)
+        public ActorFactory(Container container, Vector3 spawnPosition)
         {
-            _config = config;
             _container = container;
             _spawnPosition = spawnPosition;
         }
