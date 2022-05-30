c0d3d9c69d855fe9dcfda54b3965a3ed0091e39f
Matroskin_Cat <groznov3@gmail.com>
2022-05-30 11:51:08 +0300

    Переработка стартапа

--- Assets/Scripts/General/GameStartup.cs
+++ Assets/Scripts/General/GameStartup.cs
@@ -6,7 +6,7 @@ namespace Assets.Scripts
     {
         [Header("Configurations")]
         [SerializeField] private City _city;
-        [SerializeField][Range(10, 100)] private float _startSpeed;
+        [SerializeField][Range(10, 100)] private float _startingSpeed;
 
         private EntitySpawner _entitySpawner;
         private ChunkGenerator _chunkGenerator;
@@ -19,14 +19,16 @@ namespace Assets.Scripts
 
         private void Start()
         {
-            SpeedService.SetStartSpeed(_startSpeed);
-            _entitySpawner.Init(_city);
-            _entitySpawner.EnablePlayerCamera();
-            _entitySpawner.EnableQuadcopter();
-            _entitySpawner.EnableCarTraffic();
-            _entitySpawner.EnableAggressiveBirds();
-            _chunkGenerator.Init(_city, _entitySpawner);
+            Container chunkContainer = ContainerService.GetCreatedContainer("Chunks", _city.transform);
+            Container entityContainer = ContainerService.GetCreatedContainer("Entities", _city.transform);
+            SpeedService.SetStartSpeed(_startingSpeed);
 
+            _chunkGenerator.EnableChunks(chunkContainer);
+            _entitySpawner.EnablePlayerCamera(entityContainer);
+            _entitySpawner.EnableQuadcopter(entityContainer);
+            //_entitySpawner.EnableCarTraffic(entityContainer);
+            //_entitySpawner.EnableAggressiveBirds(entityContainer);
+            _entitySpawner.EnableNetGuys(entityContainer, _chunkGenerator);
         }
     }
 }
