5bab246285f3210bfd770d023945d81a9faa570b
Matroskin_Cat <groznov3@gmail.com>
2022-05-30 11:51:48 +0300

    Обновление сервиса контейнеров

--- Assets/Scripts/Services/ContainerService.cs
+++ Assets/Scripts/Services/ContainerService.cs
@@ -4,6 +4,14 @@ namespace Assets.Scripts
 {
     public class ContainerService : MonoBehaviour
     {
+        public static Container GetCreatedContainer(string title, Transform parent)
+        {
+            GameObject container = new GameObject(title);
+            container.transform.SetParent(parent);
+            container.transform.position = Vector3.zero;
+            return container.AddComponent<Container>();
+        }
+
         public static Container GetCreatedContainer(string title, Transform parent, Vector3 position)
         {
             GameObject container = new GameObject(title);
