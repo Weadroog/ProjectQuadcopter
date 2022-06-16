54050da8d3ae98ab870919559423b182073c9b70
MaminProgrammist <kit04048@gmail.com>
2022-06-16 15:34:17 +0300

    Новая реакция
    реакция толкания объекта

---
+++ Assets/Scripts/Actors/Entities/Reactions/PushForwardReaction.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts
+{
+    class PushForwardReaction : Reaction
+    {
+        private Mover _mover;
+
+        public PushForwardReaction(Mover mover)
+        {
+            _mover = mover;
+        }
+
+        public override void React() 
+        {
+            _mover.SetSelfSpeed(_detectableEntity.GetComponent<Mover>().SelfSpeed);
+        }
+    }
+}
