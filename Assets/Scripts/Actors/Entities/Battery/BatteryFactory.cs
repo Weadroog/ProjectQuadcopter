e367cbf92b5328f5e746f815ad4b7b30504a5fde
MaminProgrammist <kit04048@gmail.com>
2022-06-16 15:37:07 +0300

    Рефакторинг фабрики
    небольшие изменения фабрики батареек

--- Assets/Scripts/Actors/Entities/Battery/BatteryFactory.cs
+++ Assets/Scripts/Actors/Entities/Battery/BatteryFactory.cs
@@ -11,11 +11,20 @@ namespace Assets.Scripts
         public override Battery GetCreated()
         {
             Battery battery = Object.Instantiate(_config.Prefab); 
+
             Disappearer disappearer = battery.gameObject.AddComponent<Disappearer>();
-            battery.gameObject.AddComponent<Mover>();
-            battery.AddReaction<CollisionDetector, Quadcopter>(new BatteryDisappearReaction(battery));
-            
             disappearer.SetDisappearPoint(_wayMatrix.DisappearPoint);
+
+            Rotator rotator = battery.gameObject.AddComponent<Rotator>();
+            rotator.SetRotationSpeed(_config.RotationSpeed);
+
+            Mover mover = battery.gameObject.AddComponent<Mover>();
+
+            battery.AddReaction<CollisionDetector, Quadcopter>(new BatteryDisappearReaction(battery));
+            battery.AddReaction<BackDetector, AggressiveBird, Car>(new PushForwardReaction(mover)).SetDetectionDistance(10f);
+
+
+
             return battery;
         }
     }
