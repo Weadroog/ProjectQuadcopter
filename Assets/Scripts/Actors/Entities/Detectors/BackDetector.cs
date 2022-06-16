f08756f0b975636a0424519e25d5a53b97f0c31a
MaminProgrammist <kit04048@gmail.com>
2022-06-16 15:33:31 +0300

    Добавлен детектор
    Новый жопный детектор

---
+++ Assets/Scripts/Actors/Entities/Detectors/BackDetector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BackDetector : MonoBehaviour, IDetector
+    {
+        public event Action<Entity> OnDetect;
+        public event Action OnDetectAll;
+
+        private float _detectionDistance;
+        private bool _isDetection = true;
+
+        private void OnEnable() => UpdateService.OnUpdate += Detect;
+
+        public void SetDetectionDistance(float range) => _detectionDistance = range;
+
+        private void Detect()
+        {
+            if (IsTargetInRadius(out Entity target) && _isDetection)
+            {
+                OnDetectAll?.Invoke();
+                OnDetect?.Invoke(target);
+                _isDetection = false;
+            }
+
+            if (IsTargetInRadius(out target) == false && _isDetection == false)
+                _isDetection = true;
+        }
+
+        private bool IsTargetInRadius(out Entity target)
+        {
+            Ray ray = new Ray(transform.position, Vector3.forward);
+            Debug.DrawRay(ray.origin, ray.direction * _detectionDistance, Color.red);
+
+            if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit detectionInfo, _detectionDistance))
+            {
+                if (detectionInfo.collider.TryGetComponent(out target))
+                {
+                    return true;
+                }
+            }
+
+            target = null;
+            return false;
+        }
+
+        private void OnDisable() => UpdateService.OnUpdate -= Detect;
+    }
+}
