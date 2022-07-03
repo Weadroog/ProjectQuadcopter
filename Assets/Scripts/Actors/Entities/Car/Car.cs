04d25651b270808a71e00a2e7d83999a24ea6faa
MaminProgrammist <kit04048@gmail.com>
2022-07-03 13:53:27 +0300

    Изменения в скрипте
    машины теперь имеют размер по Z(!!!СВАСТОНЫ!!!)

--- Assets/Scripts/Actors/Entities/Car/Car.cs
+++ Assets/Scripts/Actors/Entities/Car/Car.cs
@@ -1,11 +1,16 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class Car : Entity
     {
         public CarColorChanger CarColorChanger { get; private set; }
+        public float Size { get; private set; }
 
-        private void Awake() => CarColorChanger = GetComponent<CarColorChanger>();
+        private void Awake() 
+        {
+            CarColorChanger = GetComponent<CarColorChanger>();
+            Size = GetComponentInChildren<Collider>().bounds.size.z;
+        } 
     }
 }
