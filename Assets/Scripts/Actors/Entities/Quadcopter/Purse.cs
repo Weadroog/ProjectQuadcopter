533c4cd67c32c20d2914cca822c65c6b7975985a
MaminProgrammist <kit04048@gmail.com>
2022-07-04 15:03:36 +0300

    Добавлен новый класс
    кошелек: хранит деньги и операции с ними

---
+++ Assets/Scripts/Actors/Entities/Quadcopter/Purse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    class Purse : ConfigReceiver<QuadcopterConfig>
+    {
+        public event Action<int> OnChanged;
+
+        private int _money;
+
+        public int Money
+        {
+            get => _money;
+
+            private set
+            {
+                _money = value;
+
+                OnChanged?.Invoke(_money);
+            }
+        }
+
+        public void AddMoney(int money) => Money += money;
+
+        public void SubtractMoney(int money) => Money -= money;
+
+        public void SetInitialAmount() => Money = _config.Money;
+    }
+}
