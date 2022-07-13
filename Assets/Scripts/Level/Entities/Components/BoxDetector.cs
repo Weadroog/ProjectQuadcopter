6dc11b06e188d689f747ad0881a9d8b4305d3f89
MaminProgrammist <kit04048@gmail.com>
2022-07-14 00:38:31 +0300

    Сделал бокс детектор
    а должен был сделать Дима, я конечно ни на что не намекаю >:(

--- Assets/Scripts/Level/Entities/Components/BoxDetector.cs
+++ Assets/Scripts/Level/Entities/Components/BoxDetector.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UnityEngine;
-using General;
 using Services;
 using Entities;
 
@@ -13,66 +12,78 @@ namespace Components
 
         private bool _isDetection = true;
         private Entity _target;
+        private float[] _xDetectionDistance = new float[2];
+        private float[] _yDetectionDistance = new float[2];
+        private float[] _zDetectionDistance = new float[2];
 
         private void OnEnable()
         {
             UpdateService.OnUpdate += Detect;
             _target = FindObjectOfType<Quadcopter>();
+
+            if (_target)
+            {
+                _xDetectionDistance[0] = _config.XDetectionDistanceLeft;
+                _xDetectionDistance[1] = _config.XDetectionDistanceRight;
+                _yDetectionDistance[0] = _config.YDetectionDistanceDown;
+                _yDetectionDistance[1] = _config.YDetectionDistanceUp;
+                _zDetectionDistance[0] = _config.ZDetectionDistanceBackward;
+                _zDetectionDistance[1] = _config.ZDetectionDistanceForward;
+            }
         }
 
         private void Detect()
         {
-            if (IsTargetInRadius() && _isDetection)
+            if (IsTargetInBox() && _isDetection)
             {
                 OnDetectAll?.Invoke();
                 OnDetect?.Invoke(_target);
                 _isDetection = false;
             }
 
-            if (IsTargetInRadius() == false && _isDetection == false)
+            if (IsTargetInBox() == false && _isDetection == false)
                 _isDetection = true;
         }
 
-        private bool IsTargetInRadius()
+        private bool IsTargetInBox()
         {
-            float ellipseRightSideValue = 1;
-            float xDistance = _config.ZDetectionDistanceForward;
-            float zDistance = _config.XDetectionDistanceLeft;
-            float yUpDistance = _config.YDetectionDistanceUp * WayMatrix.VerticalSpacing + WayMatrix.VerticalSpacing / 4;
-            float yDownDistance = Mathf.Abs(_config.YDetectionDistanceDown) * WayMatrix.VerticalSpacing + WayMatrix.VerticalSpacing / 4;
-            float xzDistance = (Mathf
-                .Pow(_target.transform.position.z - transform.position.z, 2) / Mathf
-                .Pow(zDistance, 2) + Mathf
-                .Pow(_target.transform.position.x - transform.position.x, 2) / Mathf
-                .Pow(xDistance, 2));
-            float verticalDistanceToTarget = transform.position.y - _target.transform.position.y;
-            bool isInVerticalDistance;
+            Vector3 distance = _target.transform.position - transform.position;
+            int xIndex = (int)Mathf.Clamp01(Mathf.Sign(distance.x) + 1);
+            int yIndex = (int)Mathf.Clamp01(Mathf.Sign(distance.y) + 1);
+            int zIndex = (int)Mathf.Clamp01(Mathf.Sign(distance.z) + 1);
 
-            Draw(yUpDistance, yDownDistance, zDistance, xDistance, 30);
+            #if (UNITY_EDITOR)
+            Draw();
+            #endif
 
-            isInVerticalDistance = verticalDistanceToTarget > 0 
-                ? yDownDistance >= Mathf.Abs(verticalDistanceToTarget) 
-                : yUpDistance >= Mathf.Abs(verticalDistanceToTarget);
-
-            if (xzDistance <= ellipseRightSideValue && isInVerticalDistance)
-                return true;
-
-            return false;
+            return _xDetectionDistance[xIndex] >= Mathf.Abs(distance.x) && _yDetectionDistance[yIndex] >= Mathf.Abs(distance.y) && _zDetectionDistance[zIndex] >= Mathf.Abs(distance.z);
         }
 
-        private void Draw(float yAxisUpLength, float yAxisDownLength, float zAxisLength, float xAxisLength, int pointsNumber)
+        private void Draw()
         {
-            Vector3 previousPoint = new Vector3(xAxisLength * Mathf.Sin(0) + transform.position.x, transform.position.y, zAxisLength * Mathf.Cos(0) + transform.position.z);
-            Vector3 nextPoint;
+            Vector3 pos = transform.position;
+            Vector3[] points = 
+                { 
+                pos + new Vector3(-_xDetectionDistance[0], -_yDetectionDistance[0], -_zDetectionDistance[0]),
+                pos + new Vector3(_xDetectionDistance[1], -_yDetectionDistance[0], -_zDetectionDistance[0]),
+                pos + new Vector3(_xDetectionDistance[1], -_yDetectionDistance[0], _zDetectionDistance[1]),
+                pos + new Vector3(-_xDetectionDistance[0], -_yDetectionDistance[0], _zDetectionDistance[1]),
+                pos + new Vector3(-_xDetectionDistance[0], -_yDetectionDistance[0], -_zDetectionDistance[0]),
+                pos + new Vector3(-_xDetectionDistance[0], _yDetectionDistance[1], -_zDetectionDistance[0]),
+                pos + new Vector3(_xDetectionDistance[1], _yDetectionDistance[1], -_zDetectionDistance[0]),
+                pos + new Vector3(_xDetectionDistance[1], _yDetectionDistance[1], _zDetectionDistance[1]),
+                pos + new Vector3(-_xDetectionDistance[0], _yDetectionDistance[1], _zDetectionDistance[1]),
+                pos + new Vector3(-_xDetectionDistance[0], _yDetectionDistance[1], -_zDetectionDistance[0]),
+                };
 
-            Debug.DrawLine(previousPoint + yAxisUpLength * Vector3.up, previousPoint + yAxisDownLength * Vector3.down, Color.green);
-
-            for (float i = 2 * Mathf.PI / pointsNumber; i < 2 * Mathf.PI; i += 2 * Mathf.PI / pointsNumber)
+            for (int i = 0; i < points.Length - 1; i++)
             {
-                nextPoint = new Vector3(xAxisLength * Mathf.Sin(i) + transform.position.x, transform.position.y, zAxisLength * Mathf.Cos(i) + transform.position.z);
-                Debug.DrawLine(previousPoint, nextPoint, Color.red);
-                Debug.DrawLine(nextPoint + yAxisUpLength * Vector3.up, nextPoint + yAxisDownLength * Vector3.down, Color.green);
-                previousPoint = nextPoint;
+                Debug.DrawLine(points[i], points[i+1], Color.green);
+            }
+
+            for(int i = 1; i < 4; i++)
+            {
+                Debug.DrawLine(points[i], points[i+5], Color.green);
             }
         }
 
