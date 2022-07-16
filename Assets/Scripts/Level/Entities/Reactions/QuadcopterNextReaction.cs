fbf8325053a26c0df725597c7cc10b1a7d827db9
Styopa Groznov <groznov3@gmail.com>
2022-07-16 20:33:53 +0300

    Исправлен баг с заменой коптера на другой

--- Assets/Scripts/Level/Entities/Reactions/QuadcopterNextReaction.cs
+++ Assets/Scripts/Level/Entities/Reactions/QuadcopterNextReaction.cs
@@ -12,19 +12,22 @@ namespace Reactions
         private QuadcopterConfig _config;
         private Quadcopter _quadCopter;
         private SwipeController _swipeController;
+        private float _takeNextDuration;
 
         public QuadcopterNextReaction(Quadcopter quadcopter, QuadcopterConfig config)
         {
             _config = config;
             _quadCopter = quadcopter;
             _swipeController = quadcopter.GetComponent<SwipeController>();
+            _takeNextDuration = config.MotionDuration * 3;
         }
 
         public override void React()
         {
-            _quadCopter.transform.position = _wayMatrix.GetPosition(MatrixPosition.Center) + Vector3.back * 20;
+            _quadCopter.transform.position = _wayMatrix.GetPosition(MatrixPosition.Center) + Vector3.forward * _wayMatrix.DisappearPoint;
             _swipeController.SetPosition(MatrixPosition.Center);
-            _quadCopter.transform.DOMove(_wayMatrix.GetPositionByArrayCoordinates(_swipeController.CurrentPosition), _config.MotionDuration * 2);
+            DOTween.Kill(_swipeController.transform);
+            _quadCopter.transform.DOMove(_wayMatrix.GetPositionByArrayCoordinates(_swipeController.CurrentPosition), _takeNextDuration);
         }
     }
 }
